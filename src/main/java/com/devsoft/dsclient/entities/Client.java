f799a10f691bf59bfa72c956a7bccaf3af456565
inacioads23 <jinacio11@gmail.com>
2025-02-12 15:07:45 -0300

    Update file Client.java. (Mapping the Client entity)

--- src/main/java/com/devsoft/dsclient/entities/Client.java
+++ src/main/java/com/devsoft/dsclient/entities/Client.java
@@ -2,9 +2,19 @@ package com.devsoft.dsclient.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
+	private String name;	
 	private String cpf;
 	private Double income;
 	private LocalDate birthDate;
