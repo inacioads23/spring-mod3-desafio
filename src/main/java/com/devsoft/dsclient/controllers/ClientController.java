5164fb5dd22546eb5990ff5ca415731e95b6916f
inacioads23 <jinacio11@gmail.com>
2025-02-13 09:33:53 -0300

    GET client all with pagination

--- src/main/java/com/devsoft/dsclient/controllers/ClientController.java
+++ src/main/java/com/devsoft/dsclient/controllers/ClientController.java
@@ -1,8 +1,8 @@
 package com.devsoft.dsclient.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +25,9 @@ public class ClientController {
 	}
 	
 	@GetMapping
-	public List<ClientDTO> findAll() {
-		return service.findAll();
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<ClientDTO> dto = service.findAll(pageable);
+		return dto;
 	}
 	
 
--- src/main/java/com/devsoft/dsclient/services/ClientService.java
+++ src/main/java/com/devsoft/dsclient/services/ClientService.java
@@ -1,9 +1,10 @@
 package com.devsoft.dsclient.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,8 +27,8 @@ public class ClientService {
 	}
 	
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
-		List<Client> result = repository.findAll();
-		return result.stream().map(x -> new ClientDTO(x)).toList();
+	public Page<ClientDTO> findAll(Pageable pageable){
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
 	}
 }
