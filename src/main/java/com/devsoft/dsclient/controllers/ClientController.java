63924ba09a9dbd5abb08ff5c7e0670deaabfa816
inacioads23 <jinacio11@gmail.com>
2025-02-13 09:28:34 -0300

    GET client all

--- src/main/java/com/devsoft/dsclient/controllers/ClientController.java
+++ src/main/java/com/devsoft/dsclient/controllers/ClientController.java
@@ -1,5 +1,7 @@
 package com.devsoft.dsclient.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,5 +23,11 @@ public class ClientController {
 		ClientDTO dto = service.findById(id);
 		return dto;
 	}
+	
+	@GetMapping
+	public List<ClientDTO> findAll() {
+		return service.findAll();
+	}
+	
 
 }
--- src/main/java/com/devsoft/dsclient/services/ClientService.java
+++ src/main/java/com/devsoft/dsclient/services/ClientService.java
@@ -1,9 +1,11 @@
 package com.devsoft.dsclient.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsoft.dsclient.dto.ClientDTO;
 import com.devsoft.dsclient.entities.Client;
@@ -15,11 +17,17 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> result = repository.findById(id);
 		Client client = result.get();
 		ClientDTO dto = new ClientDTO(client);
 		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll(){
+		List<Client> result = repository.findAll();
+		return result.stream().map(x -> new ClientDTO(x)).toList();
 	}
 }
