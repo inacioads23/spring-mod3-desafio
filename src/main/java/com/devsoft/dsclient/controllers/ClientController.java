2625e9fbddba331f74751d2a119402544b9d053c
inacioads23 <jinacio11@gmail.com>
2025-02-13 11:06:51 -0300

    Responses with ResponseEntity

--- src/main/java/com/devsoft/dsclient/controllers/ClientController.java
+++ src/main/java/com/devsoft/dsclient/controllers/ClientController.java
@@ -1,14 +1,18 @@
 package com.devsoft.dsclient.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsoft.dsclient.dto.ClientDTO;
 import com.devsoft.dsclient.services.ClientService;
@@ -21,21 +25,22 @@ public class ClientController {
 	private ClientService service;
 	
 	@GetMapping(value = "/{id}")
-	public ClientDTO findById(@PathVariable Long id) {
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO dto = service.findById(id);
-		return dto;
+		return ResponseEntity.ok(dto);
 	}
 	
 	@GetMapping
-	public Page<ClientDTO> findAll(Pageable pageable) {
+	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
 		Page<ClientDTO> dto = service.findAll(pageable);
-		return dto;
+		return ResponseEntity.ok(dto);
 	}
 	
 	@PostMapping
-	public ClientDTO insert(@RequestBody ClientDTO dto) {
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
 		dto = service.insert(dto);
-		return dto;
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
