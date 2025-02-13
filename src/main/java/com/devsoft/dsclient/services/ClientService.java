9b58cc3e5116081d480c21093d6f0840e71d0c90
inacioads23 <jinacio11@gmail.com>
2025-02-13 11:19:54 -0300

    Updating client with PUT

--- src/main/java/com/devsoft/dsclient/services/ClientService.java
+++ src/main/java/com/devsoft/dsclient/services/ClientService.java
@@ -35,12 +35,21 @@ public class ClientService {
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
-		copyDtoEntity(dto, entity);
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 	}
 	
-	private void copyDtoEntity(ClientDTO dto, Client entity) {
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		Client entity = repository.getReferenceById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
